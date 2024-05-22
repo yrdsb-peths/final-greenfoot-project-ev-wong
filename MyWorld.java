f8a6c57e392d7c92b7dd6223a60bdc46b6d28210
ev-wong <167326615+ev-wong@users.noreply.github.com>
2024-05-22 17:58:51 -0400

    Quick Commit
    will begin project friday

--- MyWorld.java
+++ MyWorld.java
@@ -1,10 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * First Commit Test
  */
 public class MyWorld extends World
 {
