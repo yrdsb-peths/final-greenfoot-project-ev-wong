3cd9e1208da4caea04ed6dc4498b988112a60554
ev-wong <167326615+ev-wong@users.noreply.github.com>
2024-05-29 17:02:18 -0400

    table

--- PokerWorld.java
+++ PokerWorld.java
@@ -17,5 +17,28 @@ public class PokerWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        CardBack cardBack = new CardBack();
+        addObject(cardBack,270,363);
+        CardBack cardBack2 = new CardBack();
+        addObject(cardBack2,330,363);
+        CardBack cardBack3 = new CardBack();
+        addObject(cardBack3,300,160);
+        CardBack cardBack4 = new CardBack();
+        addObject(cardBack4,350,160);
+        CardBack cardBack5 = new CardBack();
+        addObject(cardBack5,250,160);
+        CardBack cardBack6 = new CardBack();
+        addObject(cardBack6,200,160);
+        CardBack cardBack7 = new CardBack();
+        addObject(cardBack7,400,160);
     }
 }
